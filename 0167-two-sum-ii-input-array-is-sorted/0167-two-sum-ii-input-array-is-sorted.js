540107528be659f61d39759c76c1ae0eb3565ccb
Cono <111506027+everydaycono@users.noreply.github.com>
2023-08-27 22:13:11 +0900

    Time: 56 ms (72.40%), Space: 43.2 MB (17.94%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -4,11 +4,18 @@
  * @return {number[]}
  */
 var twoSum = function(numbers, target) {
-    for (let i = 0; i < numbers.length - 1; i++) {
-        for (let j = i + 1; j < numbers.length; j++) {
-            if (numbers[i] + numbers[j] === target) {
-                return [i + 1, j + 1];
-            }
+    let left = 0;
+    let right = numbers.length - 1;
+    
+    while (left < right) {
+        const sum = numbers[left] + numbers[right];
+        
+        if (sum === target) {
+            return [left + 1, right + 1];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
         }
     }
 };
